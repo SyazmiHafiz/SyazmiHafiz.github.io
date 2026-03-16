6d848800f2aa1615cbfb4c92f1a57c95cbb903d1
SyazmiHafiz <syazmihafiz97x@gmail.com>
2026-03-16 11:32:05 +0800

    update

---
+++ syazmi&nurin/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'syazmi-nurin-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/menikah.css',
+  './css/jquery.countdown.css',
+  './js/menikah.js',
+  './js/jquery.coundown.js',
+  './image/favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
